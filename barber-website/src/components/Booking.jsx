06a4eb197fac93456dee84813f2e2be3dacbbe7b
Hristov,Zhan Z. <i500184@fhict.nl>
2026-06-15 15:03:15 +0300

    starting the booking section

--- barber-website/src/components/Booking.jsx
+++ barber-website/src/components/Booking.jsx
@@ -1,7 +1,40 @@
 function Booking() {
   return (
-    <section>
-      <h2>Booking</h2>
+    <section className="booking-section" id="booking">
+      <div className="booking-content">
+        <div className="booking-text">
+
+          <h2>
+            BOOK AN <br />
+            <span>APPOINT</span>
+            <br />
+            MENT.
+          </h2>
+
+          <p className="booking-description">
+            We take pride in delivering a premium barbering experience from the
+            moment you walk through our doors. Select your preferred barber and
+            time at your convenience. Reschedules require at least 24 hours
+            notice.
+          </p>
+
+          <button className="booking-button">BOOK NOW</button>
+        </div>
+
+        <div className="booking-images">
+          <img
+            className="booking-img booking-img-left"
+            src="/cuttinghair.png"
+            alt="Barber cutting hair"
+          />
+
+          <img
+            className="booking-img booking-img-right"
+            src="/thecut.png"
+            alt="Finished haircut"
+          />
+        </div>
+      </div>
     </section>
   );
 }
